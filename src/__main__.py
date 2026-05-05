3ee39a964e8b466cf51a5223456b67abea420767
ovega14 <vega00087@gmail.com>
2026-05-05 15:02:47 -0500

    implemented `__main__` script

---
+++ src/__main__.py
@@ -0,0 +1,18 @@
+import importlib
+import runpy
+import warnings
+# silence already imported warning from runpy
+warnings.simplefilter('ignore')
+
+
+def run_submodule(name: str) -> None:
+    """Executes a submodule as a .py file."""
+    full_name = importlib.util.find_spec('.'+name, package=__package__).name
+    print(f'Running submodule {name} ({full_name})...')
+    runpy.run_module(full_name, run_name='__main__')
+    print()
+
+
+run_submodule('consistency')
+run_submodule('dataset')
+run_submodule('util')
