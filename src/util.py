2fc4d0142c78a758c1e573bbb1b140f5540b6fec
ovega14 <vega00087@gmail.com>
2026-05-04 21:21:24 -0500

    implemented `update_ema`

--- src/util.py
+++ src/util.py
@@ -12,3 +12,18 @@ def grab(inp):
 def wrap(inp):
     """Wraps an input into the interval [0, 2\pi)."""
     return (inp + math.pi) % (2*math.pi) - math.pi
+
+
+@torch.no_grad()
+def update_ema(ema_model, online_model, mu=0.95) -> None:
+    """
+    Updates the parameters of the ema_model according to
+    EMA update: theta_ema = mu * theta_ema + (1-mu) * theta_online
+
+    Args:
+        ema_model (Module): Consistency model with EMA parameters
+        online_model (Module): Consistency model with updating parameters
+        mu (float): EMA decay factor. Default: 0.95
+    """
+    for ema_p, online_p in zip(ema_model.parameters(), online_model.parameters()):
+        ema_p.data.mul_(mu).add_(online_p.data, alpha=1 - mu)
